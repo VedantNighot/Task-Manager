f5b091a962ffd617d5392def5f6a878de7b51a16
Vedant Nighot <vedantnighotzzz1@gmail.com>
2026-02-02 21:56:57 +0530

    error fixed

--- frontend/Task-Manager/src/components/layouts/Progress.jsx
+++ frontend/Task-Manager/src/components/layouts/Progress.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import { HiArrowLeftCircle } from "react-icons/hi2";
 
-const Progress = ({progress,status}) => {
+const Progress = ({ progress, status }) => {
     const getColor = () => {
-        switch(status){
+        switch (status) {
             case 'In Progress':
-                return 'text-cyan-500 bg-cyan-50 border border-cyan-500/10'
+                return 'bg-cyan-500';
             case 'Completed':
-                return 'text-indigo-500 bg-indigo-50 border-indigo-500/10'
+                return 'bg-lime-500';
             default:
-                return 'text-violet-500 bg-violet-50 border-violet-500/10'
+                return 'bg-violet-500';
         }
     }
-  return (
+}
+return (
     <div className="w-full bg-gray-200 rounded-full h-1.5">
-        <div 
-            className={`${getColor()} h-1.5 rounded-full text-center text-xs font-medium `} style={{width:`${progress}%`}}>
+        <div
+            className={`${getColor()} h-1.5 rounded-full text-center text-xs font-medium `} style={{ width: `${progress}%` }}>
         </div>
     </div>
-  )
+)
 }
 
 export default Progress;
